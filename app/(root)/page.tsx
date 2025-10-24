222b1f9ed676dafb13a6e5fa305fd8f20a05ff14
Minh Nguyen <nhminh.ng@gmail.com>
2025-10-23 22:59:49 -0500

    Implement daily new summary

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,6 +7,7 @@ import {
     MARKET_OVERVIEW_WIDGET_CONFIG,
     TOP_STORIES_WIDGET_CONFIG
 } from "@/lib/constants";
+import {sendDailyNewsSummary} from "@/lib/inngest/functions";
 
 const Home = () => {
 
