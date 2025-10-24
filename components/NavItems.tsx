6d4d52e1d5dcfad46544b83d8dc5ea1e4c008606
Minh Nguyen <nhminh.ng@gmail.com>
2025-10-24 15:47:55 -0500

    Implement search stocks

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 import {NAV_ITEMS} from "@/lib/constants";
 import {usePathname} from "next/navigation";
 import Link from "next/link";
+import SearchCommand from "@/components/SearchCommand";
 
-const NavItems = () => {
+const NavItems = ({initialStocks}: { initialStocks: StockWithWatchlistStatus[]}) => {
 
     const pathname: string = usePathname()
 
@@ -14,14 +15,24 @@ const NavItems = () => {
     }
     return (
         <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
-            {NAV_ITEMS.map(({href, label}) => (
-                <li key={href}>
+            {NAV_ITEMS.map(({ href, label }) => {
+                if (label === 'Search')
+                    return (
+                    <li key="search-trigger">
+                        <SearchCommand
+                            renderAs="text"
+                            label="Search"
+                            initialStocks={initialStocks}
+                        />
+                    </li>
+                    )
+                return <li key={href}>
                     <Link href={href} className={`hover:text-yellow-500 transition-colors
                     ${isActive(href) ? 'text-gray-100' : ''}`}>
                         {label}
                     </Link>
                 </li>
-            ))}
+            })}
         </ul>
     )
 }
